7754a48b58287d70f4ba857e69738de17aaaf36a
cfuselier74 <33632427+cfuselier74@users.noreply.github.com>
2020-10-13 17:10:52 -0700

    fixed test

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) < 2 {
 		printError(errInvalidArguments)
 		return
 	}
@@ -38,12 +38,11 @@ func main() {
 			convertToCelsius(originValue)
 		}
 
+		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 		_, err = fmt.Scanln(&shouldConvertAgain)
 		if err != nil {
 			printError(errReadingInput)
 		}
-
-		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
 		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
